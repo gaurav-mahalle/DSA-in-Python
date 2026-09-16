3094bf0fc5db0840f4a4f52c6084a95485b4d6e2
gaurav-mahalle <gauravmahalle2020@gmail.com>
2026-09-16 18:55:35 +0530

    BasicsQuestions/calculator.py

---
+++ BasicsQuestions/calculator.py
@@ -0,0 +1,18 @@
+num1 = int(input())
+num2 = int(input())
+operator = input()
+
+if operator == "+":
+    print(num1 + num2)
+
+elif operator == "-":
+    print(num1 - num2)
+
+elif operator == "*":
+    print(num1 * num2)
+
+elif operator == "/":
+    print(num1 / num2)
+
+else:
+    print("Invalid operator")
